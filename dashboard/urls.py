3238c5b8fe669864b54523ec5ee4fe13646c38f9
Kaio Oliveira <kaiohenrique2601@gmail.com>
2021-03-30 18:57:53 -0300

    fix

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,10 @@
-# from .views import DashBoardView,AverageSalesView,BestSellingsView,LessSoldView,SalesView
-# from django.urls import path
+from .views import DashBoardView,AverageSalesView,BestSellingsView,LessSoldView,SalesView
+from django.urls import path
 
-# urlpatterns = [
-#     path('dashboard/', DashBoardView.as_view()),
-#     path('average-sales/', AverageSalesView.as_view()),
-#     path('best-sellers-data/',BestSellingsView.as_view()),
-#     path('less-sold-data/',LessSoldView.as_view()),
-#     path('sales/',SalesView.as_view()),
-# ]
+urlpatterns = [
+    path('dashboard/', DashBoardView.as_view()),
+    path('average-sales/', AverageSalesView.as_view()),
+    path('best-sellers-data/',BestSellingsView.as_view()),
+    path('less-sold-data/',LessSoldView.as_view()),
+    path('sales/',SalesView.as_view()),
+]
--- menu/views.py
+++ menu/views.py
@@ -1,34 +1,34 @@
-# from django.shortcuts import render, redirect
-# from django.template.response import TemplateResponse
-# from django.contrib.auth import authenticate
-# from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import render, redirect
+from django.template.response import TemplateResponse
+from django.contrib.auth import authenticate
+from django.contrib.contenttypes.models import ContentType
 
-# from rest_framework import status
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# import json
+import json
 
-# from .models import Item
-# from .serializers import ItemSerializer
-# from .forms import ItemForm
-
-
-# class MenuView(APIView):
-#     def get(self, request):
-#         items = json.dumps(ItemSerializer(Item.objects.all(), many=True).data)
-#         return TemplateResponse(request, 'menu.html', {'items': items})
-
-#     def post(self, request):
-#         return Response(request.data)
+from .models import Item
+from .serializers import ItemSerializer
+from .forms import ItemForm
 
 
-# class InsertItem(APIView):
-#     def get(self, request):
-#         return render(request, 'item_form.html')
+class MenuView(APIView):
+    def get(self, request):
+        items = json.dumps(ItemSerializer(Item.objects.all(), many=True).data)
+        return TemplateResponse(request, 'menu.html', {'items': items})
 
-#     def post(self, request):
-#         item = Item.objects.create(
-#             **{key: request.data[key] for key in request.data if key != 'csrfmiddlewaretoken'})
-#         new_item = ItemSerializer(item).data
-#         return redirect('../menu/')
+    def post(self, request):
+        return Response(request.data)
+
+
+class InsertItem(APIView):
+    def get(self, request):
+        return render(request, 'item_form.html')
+
+    def post(self, request):
+        item = Item.objects.create(
+            **{key: request.data[key] for key in request.data if key != 'csrfmiddlewaretoken'})
+        new_item = ItemSerializer(item).data
+        return redirect('../menu/')
