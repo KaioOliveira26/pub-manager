cc0b40c1fa7b1c415aa1f710a6971fc94657f00f
Kaio Oliveira <kaiohenrique2601@gmail.com>
2021-03-31 19:32:03 -0300

    fix

--- pub_manager/settings.py
+++ pub_manager/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'e+oa=)w*eb@i2gho_$m0op6pg3!d=e^lyokc%3chd%ga9zh!_q'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['pub-mannager.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
