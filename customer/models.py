b4a4f3043aa6872da5f0b440a375c45ba404111e
Kaio Oliveira <kaiohenrique2601@gmail.com>
2021-04-01 14:46:50 -0300

    feat mask for number and cpf

--- customer/models.py
+++ customer/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length =50 )
-    cpf = models.CharField(max_length =12 )
+    cpf = models.CharField(max_length =12, unique=True )
     phone = models.CharField(max_length = 12 )
--- customer/templates/user_modal_scripts.js
+++ customer/templates/user_modal_scripts.js
@@ -13,7 +13,6 @@ $('#btnSaveUser').click(()=>{
     
         success: function (response) {
           const costumer = response
-          console.log(costumer)
           $('#textCustomerId').text(`Numero do Cliente: ${costumer.id}`)
           $('#textCustomerName').text(`Nome do Cliente: ${costumer.name.replace('[','').replace(']','').replace("'","").replace("'","")}`)
           $('#textCustomerCPF').text(`CPF do Cliente: ${costumer.cpf.replace('[','').replace(']','').replace("'","").replace("'","")}`)
